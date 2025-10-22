6060ccd8abe6b03fb7163ebc87716f74b58d1721
CYW <chenyuw2019@gmail.com>
2025-10-22 17:02:54 -0400

    Answered 32 - this method works, but I still feel like using a new array would be better, because its logic is simpler

--- Automaton.java
+++ Automaton.java
@@ -46,16 +46,16 @@ public class Automaton
      */
     public void update()
     {
-        // Build the new state in a separate array.
-        int[] nextState = new int[state.length];
-        // Naively update the state of each cell
-        // based on the state of its two neighbors.
+        int left = 0;
+        int center = state[0];
+        int right;
         for(int i = 0; i < state.length; i++) {
-            int left, center, right;
-            left = (i == 0) ? 0 : state[i - 1];
             right = (i + 1 < state.length) ? state[i + 1] : 0;
+            int newValue = (left + center + right) % 2;
+            left = center;
+            center = right;
+            state[i] = newValue;
         }
-        state = nextState;
     }
     
     /**
